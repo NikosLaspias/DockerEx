fb8b1e38e596dde094578855baa4b72b80edd860
EleftheriaKoutsiouri <147153025+EleftheriaKoutsiouri@users.noreply.github.com>
2023-12-03 13:41:08 +0200

    Add files via upload
    Image class

--- Image.java
+++ Image.java
@@ -10,15 +10,21 @@ import java.io.IOException;
 import java.util.List;
 
 public class Image implements AutoCloseable {
+
+    // Docker client and configuration objects
     private DockerClient dockerClient;
     private DefaultDockerClientConfig config;
 
+    // Constructor to initialize Docker client and configuration
     public Image() {
+        // Set Docker host configuration
         this.config = DefaultDockerClientConfig.createDefaultConfigBuilder()
                 .withDockerHost("tcp://localhost:2375").build();
+        // Build Docker client using the configuration
         this.dockerClient = DockerClientBuilder.getInstance(config).build();
     }
 
+    // Method to pull an image if it does not exist locally
     public void pullImageIfNotExists(String imageName) {
         List<com.github.dockerjava.api.model.Image> images = dockerClient.listImagesCmd().withImageNameFilter(imageName)
                 .exec();
@@ -31,10 +37,12 @@ public class Image implements AutoCloseable {
         }
     }
 
+    // Getter method to retrieve Docker host information
     public String getImageName() {
         return config.getDockerHost().getHost();
     }
 
+    // Private method to pull a Docker image
     private void pullImage(String imageName) {
         try {
             dockerClient.pullImageCmd(imageName).exec(null);
@@ -46,10 +54,12 @@ public class Image implements AutoCloseable {
         }
     }
 
+    // Method to list Docker images
     public void listImages() {
         List<com.github.dockerjava.api.model.Image> images = dockerClient.listImagesCmd().exec();
         System.out.println("List of Docker images:");
         for (com.github.dockerjava.api.model.Image image : images) {
+            // Display image information
             System.out.println("Image ID: " + image.getId());
             System.out.println("Repo Tags: " + String.join(", ", image.getRepoTags()));
             System.out.println("Created: " + image.getCreated());
@@ -58,18 +68,22 @@ public class Image implements AutoCloseable {
         }
     }
 
+    // Method to search for Docker images using a search term
     public void searchImages(String searchTerm) {
         dockerClient.searchImagesCmd(searchTerm).exec().forEach(result -> {
+            // Display search results
             System.out.println("Image Name: " + result.getName());
             System.out.println("Description: " + result.getDescription());
             System.out.println("------------------------");
         });
     }
 
+    // Method to inspect a Docker image
     public InspectImageResponse inspectImage(String imageId) {
         return dockerClient.inspectImageCmd(imageId).exec();
     }
 
+    // Method to remove a Docker image
     public void removeImage(String imageId) {
         try {
             dockerClient.removeImageCmd(imageId).exec();
@@ -79,21 +93,23 @@ public class Image implements AutoCloseable {
         }
     }
 
+    // Method to show pull history for a Docker image
     public void showPullHistory(String imageName) {
         List<com.github.dockerjava.api.model.Image> images = dockerClient.listImagesCmd().withImageNameFilter(imageName)
                 .exec();
         for (com.github.dockerjava.api.model.Image image : images) {
+            // Display pull history information
             System.out.println("Pull History for Image ID " + image.getId());
-
             ContainerConfig containerConfig = inspectImage(image.getId()).getContainerConfig();
-
             System.out.println("Commands executed during pull: " + containerConfig.getCmd());
             System.out.println("------------------------");
         }
     }
 
+    // AutoCloseable interface method for proper resource cleanup
     @Override
     public void close() {
+        // Close the Docker client if it's not null
         if (dockerClient != null) {
             try {
                 dockerClient.close();
