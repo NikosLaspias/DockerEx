620d1dbc845d83e8e2798644fae558609cab2ea2
EleftheriaKoutsiouri <147153025+EleftheriaKoutsiouri@users.noreply.github.com>
2023-12-03 13:41:56 +0200

    Add files via upload
    Γραφημα

--- MeasurementChart.java
+++ MeasurementChart.java
@@ -15,30 +15,32 @@ public class MeasurementChart extends JFrame {
 
     private ContainerMeasurement[] containerMeasurements;
 
+    // Constructor to initialize the MeasurementChart
     public MeasurementChart(String title, ContainerMeasurement[] containerMeasurements) {
         super(title);
         this.containerMeasurements = containerMeasurements;
 
-        // Δημιουργία dataset από τα δεδομένα των μετρήσεων
+        // Create dataset from measurement data
         CategoryDataset dataset = createDataset();
 
-        // Δημιουργία του γραφήματος
+        // Create the chart
         JFreeChart chart = ChartFactory.createBarChart(
-                "Container Measurements", // Τίτλος γραφήματος
-                "Containers", // Ετικέτα άξονα x
-                "Count", // Ετικέτα άξονα y
+                "Container Measurements", // Chart title
+                "Containers", // X-axis label
+                "Count", // Y-axis label
                 dataset);
 
-        // Προσθήκη του γραφήματος στο πάνελ
+        // Add the chart to the panel
         ChartPanel chartPanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new Dimension(560, 370));
         setContentPane(chartPanel);
     }
 
+    // Method to create the dataset from container measurements
     private CategoryDataset createDataset() {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
-        // Προσθήκη δεδομένων από τη λίστα των μετρήσεων
+        // Add data from the list of measurements
         for (ContainerMeasurement measurement : containerMeasurements) {
             dataset.addValue(1, "Containers", measurement.getId());
         }
